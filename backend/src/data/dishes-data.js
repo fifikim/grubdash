76a1b12c5d4bc135415b832f926024bc63f2313d
Sophia Kim <mail@fifikim.com>
2022-01-01 13:11:28 -0500

    copy backend folder from backend repo to set up monorepo

---
+++ backend/src/data/dishes-data.js
@@ -0,0 +1,36 @@
+module.exports = [
+  {
+    id: "3c637d011d844ebab1205fef8a7e36ea",
+    name: "Broccoli and beetroot stir fry",
+    description: "Crunchy stir fry featuring fresh broccoli and beetroot",
+    price: 15,
+    image_url:
+      "https://149366112.v2.pressablecdn.com/wp-content/uploads/2019/03/screen-shot-2019-03-12-at-11-43-18-am.png",
+  },
+  {
+    id: "90c3d873684bf381dfab29034b5bba73",
+    name: "Falafel and tahini grain bowl",
+    description: "A warm grain bowl filled with falafel and tahini",
+    price: 6,
+    image_url:
+      "https://dishingouthealth.com/wp-content/uploads/2021/01/FalafelBowls_Square.jpg",
+  },
+  {
+    id: "d351db2b49b69679504652ea1cf38241",
+    name: "Dolcelatte and caramelized onion pappardelle",
+    description:
+      "Pappardelle topped with a blend of dolcelatte and caramelized onion",
+    price: 19,
+    image_url:
+      "https://www.cookingbites.com/attachments/fullsizeoutput_70be-jpeg.58402/",
+  },
+  {
+    id: "d352b98989b69679504652ea1cf67346",
+    name: "Spicy salmon poke bowl",
+    description:
+      "Sockeye salmon tossed with a zesty yuzu seaweed salad",
+    price: 16,
+    image_url:
+      "https://www.jessicagavin.com/wp-content/uploads/2016/06/sockeye-salmon-poke-spicy-soy-sauce_1200.jpg",
+  },
+];
